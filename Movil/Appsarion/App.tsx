ecbbe3a4ab24df59562a69f80e2ccb830db31e9b
Hernan Quijano <hernan.quijano@unillanos.edu.co>
2025-10-20 09:48:24 -0500

    Upload doc privacy policy

--- Movil/Appsarion/App.tsx
+++ Movil/Appsarion/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View } from 'react-native';
-import Navigation from './navigation';
-import {Provider as ReduxProvider} from 'react-redux';
-import {store} from './store/index';
-import { registerRootComponent } from 'expo';
+// import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import {StyleSheet, Text, View } from 'react-native';
+// import Navigation from './navigation';
+// import {Provider as ReduxProvider} from 'react-redux';
+// import {store} from './store/index';
+// import { registerRootComponent } from 'expo';
 
-const App =() =>{
+// const App =() =>{
+//   return (
+//     <ReduxProvider store={store}>
+//         <Navigation/>
+//         <StatusBar style='auto'/>
+//       </ReduxProvider>
+//     );
+// };
+
+// registerRootComponent(App);
+
+// export default App;
+
+import Navigation from './navigation';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './store';
+
+export default function App() {
   return (
     <ReduxProvider store={store}>
-        <Navigation/>
-        <StatusBar style='auto'/>
-      </ReduxProvider>
-    );
-};
-
-registerRootComponent(App);
-
-export default App;
+      <Navigation />
+    </ReduxProvider>
+  );
+}
